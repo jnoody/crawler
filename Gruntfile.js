8533db963a4139f06b8b8b75a514e10d910767ed
Joshua Kaminetsky <joshua.kaminetsky@twcable.com>
2014-05-04 14:25:18 -0600

    react build

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,46 @@ module.exports = function (grunt) {
                     keepalive: true
                 }
             }
+        },
+        react: {
+            'dynamic_mappings': {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/jsx',
+                        src: ['**/*.jsx'],
+                        dest: 'src/www/jsx',
+                        ext: '.js'
+                    }
+                ]
+            }
+        },
+        exec: {
+            clean: {
+                cmd: function () {
+                    return 'rm -irf build/';
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        cwd: 'src/www',
+                        expand: true,
+                        src: '**',
+                        dest: 'build'
+                    }
+                ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-react');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', ['connect']);
+    grunt.registerTask('build', ['exec:clean', 'react', 'copy']);
+    grunt.registerTask('default', ['build', 'connect']);
 };
