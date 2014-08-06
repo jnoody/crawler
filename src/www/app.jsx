0b21f657bb4596cd049d3ce3791de3b6a63cacd9
Joshua Kaminetsky <joshua.kaminetsky@twcable.com>
2014-08-06 16:53:48 -0600

    now redirecting #! to _escaped_fragment_

--- src/www/app.jsx
+++ src/www/app.jsx
@@ -4,11 +4,56 @@ var React = require('./react');
 var $ = require('jquery');
 var Navigator = require('./Navigator.jsx');
 var Viewer = require('./Viewer.jsx');
+var stringUtil = require('./stringUtil');
+
+var urlPartsRegex = /(http[s]?:\/\/)([^:\/?#]*)([^\/?#]*)([^?#]*)([^#]*)/g;
+
+function escapeFragment(url) {
+    /*
+     * convert any hash params to escaped fragments as google would do
+     *
+     * RexEx positions:
+     * 0: empty
+     * 1: protocol including ://
+     * 2: host
+     * 3: port including :
+     * 4: path
+     * 5: query
+     * 6: hash
+     */
+    var urlParts = url.split(urlPartsRegex);
+
+    var hash = urlParts[6];
+
+    if (hash.indexOf('#!') === 0) {
+        hash = hash.substring(2);
+
+        var hasQuery = urlParts[5].length;
+
+        if (hasQuery) {
+            urlParts[5] += '&';
+        } else {
+            urlParts[5] = '?';
+        }
+
+        urlParts[5] += '_escaped_fragment_=' + stringUtil.encodeUriComponent(hash);
+
+        url =
+            urlParts[0] +
+            urlParts[1] +
+            urlParts[2] +
+            urlParts[3] +
+            urlParts[4] +
+            urlParts[5];
+    }
+
+    return url;
+}
 
 var CrawlerApp = React.createClass({
     getInitialState: function () {
         return {
-            url: 'http://localhost'
+            url: 'http://localhost:8000?foo=bar#!foo=baz'
         };
     },
 
@@ -64,7 +109,7 @@ var CrawlerApp = React.createClass({
 
     updateHtml: function (url) {
         $.ajax({
-            url: url,
+            url: escapeFragment(url),
             success: this.htmlDidUpdate,
             error: this.htmlDidFail
         });
---
+++ src/www/stringUtil.js
@@ -0,0 +1,5 @@
+/* globals encodeURIComponent */
+
+module.exports = {
+    encodeUriComponent: encodeURIComponent
+};
