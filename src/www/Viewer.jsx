a2a3c719a3ac1dc22e184e0a9f7da8c57357a7f4
Joshua Kaminetsky <joshua.kaminetsky@twcable.com>
2014-08-08 15:42:39 -0600

    crawler honors meta fragment content

--- src/www/Viewer.jsx
+++ src/www/Viewer.jsx
@@ -2,10 +2,6 @@
 
 var React = require('./react');
 var $ = require('jquery');
-var _ = require('./lodash');
-var stringUtil = require('./stringUtil');
-
-var escapedFragmentKey = '_escaped_fragment_';
 
 module.exports = React.createClass({
     render: function () {
@@ -18,85 +14,22 @@ module.exports = React.createClass({
     },
 
     componentDidMount: function () {
-        this.updateHtml(this.props.html);
+        this.updateDom(this.props.dom);
     },
 
     shouldComponentUpdate: function (nextProps, nextState) {
-        if (nextProps.html !== this.props.html) {
-            this.updateHtml(nextProps.html);
+        if (nextProps.dom !== this.props.dom) {
+            this.updateDom(nextProps.dom);
         }
 
         return false;
     },
 
-    updateHtml: function (html) {
-        var $container = this.getContainer(html);
-
-        // do we have a meta fragment
-        var hasMetaFragment = $container.find('meta[name="fragment"][content="!"]').length;
-
-        // are we already browsing to an escaped fragment
-        var urlParts = stringUtil.splitUrl(this.props.url);
-        var queryMap = stringUtil.toQueryMap(urlParts[5]);
-        var hasEscapedFragment = _.findWhere(queryMap, { key: escapedFragmentKey });
-
-        if (hasMetaFragment && !hasEscapedFragment) {
-            
-        } else {
-            $(this.getDOMNode()).contents().find('body')
-                .empty()
-                .append($container.html());
-        }
-    },
-
-    getContainer: function (html) {
-        var $container = $('<div></div>');
-
-        $container
-            .append(html);
-
-        this.blockJavaScript($container);
-        this.toEscapedFragment($container);;
-
-        return $container;
-    },
-
-    toEscapedFragment: function ($container) {
-        $container
-            .find('a[href*=\\#\\!]')
-            .each(function (i, a) {
-                var $a = $(a)
-                $a.attr('href', $a.attr('href').replace('#!', escapedFragmentKey))
-            });
-    },
-
-    blockJavaScript: function ($container) {
-        $container
-            .find('script')
-            .remove();
-
-        $container.find('*[onclick]').removeAttr('onclick');
-        $container.find('*[dblclick]').removeAttr('dblclick');
-        $container.find('*[onmousedown]').removeAttr('onmousedown');
-        $container.find('*[onmousemove]').removeAttr('onmousemove');
-        $container.find('*[onmouseover]').removeAttr('onmouseover');
-        $container.find('*[onmouseout]').removeAttr('onmouseout');
-        $container.find('*[onmouseup]').removeAttr('onmouseup');
-        $container.find('*[onkeypress]').removeAttr('onkeypress');
-        $container.find('*[onkeydown]').removeAttr('onkeydown');
-        $container.find('*[onkeyup]').removeAttr('onkeyup');
-        $container.find('*[onabort]').removeAttr('onabort');
-        $container.find('*[onerror]').removeAttr('onerror');
-        $container.find('*[onload]').removeAttr('onload');
-        $container.find('*[onresize]').removeAttr('onresize');
-        $container.find('*[onscroll]').removeAttr('onscroll');
-        $container.find('*[onunload]').removeAttr('onunload');
-        $container.find('*[onblur]').removeAttr('onblur');
-        $container.find('*[onchange]').removeAttr('onchange');
-        $container.find('*[onfocus]').removeAttr('onfocus');
-        $container.find('*[onreset]').removeAttr('onreset');
-        $container.find('*[onselect]').removeAttr('onselect');
-        $container.find('*[onsubmit]').removeAttr('onsubmit');
+    updateDom: function (dom) {
+        $(this.getDOMNode())
+            .contents()
+            .find('body')
+            .empty()
+            .append(dom);
     }
 });
-
--- src/www/app.jsx
+++ src/www/app.jsx
@@ -2,20 +2,18 @@
 
 var React = require('./react');
 var $ = require('jquery');
+var _ = require('./lodash');
 var Navigator = require('./Navigator.jsx');
 var Viewer = require('./Viewer.jsx');
 var stringUtil = require('./stringUtil');
 
+var escapedFragmentKey = '_escaped_fragment_';
+
 var CrawlerApp = React.createClass({
     getInitialState: function () {
         return {
-            url: 'http://localhost:8000?foo=bar'
-        };
-    },
-
-    getDefaultProps: function () {
-        return {
-            html: '<!doctype html><html><body><div>loading page...</div></body></html>'
+            url: 'http://localhost:8000?foo=bar#!baz=qux',
+            dom: $('<div>loading...</div>')
         };
     },
 
@@ -28,8 +26,7 @@ var CrawlerApp = React.createClass({
                     onUrlSubmit={ this.handleUrlSubmit }
                 />
                 <Viewer
-                    url={ this.state.url }
-                    html={ this.props.html }
+                    dom={ this.state.dom }
                 />
             </div>
         );
@@ -40,7 +37,7 @@ var CrawlerApp = React.createClass({
     },
 
     shouldComponentUpdate: function (nextProps, nextState) {
-        if (nextProps.html !== this.props.html) {
+        if (nextState.dom !== this.state.dom) {
             return true;
         }
 
@@ -58,12 +55,19 @@ var CrawlerApp = React.createClass({
     },
 
     htmlDidUpdate: function (res) {
-        this.setProps({
-            html: res
-        });
+        var dom = this.getDom(res);
+
+        if (!this.isUrlUpdating(dom)) {
+            this.setState({
+                dom: dom
+            });
+        }
     },
 
     htmlDidFail: function () {
+        this.setState({
+            dom: $('<div>Request for ' + this.state.url + ' failed.</div>')
+        });
     },
 
     updateHtml: function (url) {
@@ -72,6 +76,92 @@ var CrawlerApp = React.createClass({
             success: this.htmlDidUpdate,
             error: this.htmlDidFail
         });
+    },
+
+    isUrlUpdating: function (dom) {
+        // do we have a meta fragment
+        var hasMetaFragment = dom.find('meta[name="fragment"][content="!"]').length;
+
+        // are we already browsing to an escaped fragment
+        var urlParts = stringUtil.splitUrl(this.state.url);
+        var queryMap = stringUtil.toQueryMap(urlParts[5]);
+        var hasEscapedFragment = _.findWhere(queryMap, { key: escapedFragmentKey });
+
+        if (hasMetaFragment && !hasEscapedFragment) {
+            queryMap.push({
+                key: '_escaped_fragment_',
+                value: urlParts[6]
+            });
+            var newQuery =
+                _.map(queryMap, function (pair) {
+                    return pair.key + '=' + pair.value;
+                })
+                .join('&');
+
+            var escapedFragmentUrl =
+                urlParts[0] +
+                urlParts[1] +
+                urlParts[2] +
+                urlParts[3] +
+                urlParts[4] +
+                '?' + newQuery;
+
+            this.setState({ url: escapedFragmentUrl });
+
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    getDom: function (html) {
+        var $container = $('<div></div>');
+
+        $container
+            .append(html);
+
+        this.blockJavaScript($container);
+        this.toEscapedFragment($container);;
+
+        return $container;
+    },
+
+    blockJavaScript: function ($container) {
+        $container
+            .find('script')
+            .remove();
+
+        $container.find('*[onclick]').removeAttr('onclick');
+        $container.find('*[dblclick]').removeAttr('dblclick');
+        $container.find('*[onmousedown]').removeAttr('onmousedown');
+        $container.find('*[onmousemove]').removeAttr('onmousemove');
+        $container.find('*[onmouseover]').removeAttr('onmouseover');
+        $container.find('*[onmouseout]').removeAttr('onmouseout');
+        $container.find('*[onmouseup]').removeAttr('onmouseup');
+        $container.find('*[onkeypress]').removeAttr('onkeypress');
+        $container.find('*[onkeydown]').removeAttr('onkeydown');
+        $container.find('*[onkeyup]').removeAttr('onkeyup');
+        $container.find('*[onabort]').removeAttr('onabort');
+        $container.find('*[onerror]').removeAttr('onerror');
+        $container.find('*[onload]').removeAttr('onload');
+        $container.find('*[onresize]').removeAttr('onresize');
+        $container.find('*[onscroll]').removeAttr('onscroll');
+        $container.find('*[onunload]').removeAttr('onunload');
+        $container.find('*[onblur]').removeAttr('onblur');
+        $container.find('*[onchange]').removeAttr('onchange');
+        $container.find('*[onfocus]').removeAttr('onfocus');
+        $container.find('*[onreset]').removeAttr('onreset');
+        $container.find('*[onselect]').removeAttr('onselect');
+        $container.find('*[onsubmit]').removeAttr('onsubmit');
+    },
+
+    toEscapedFragment: function ($container) {
+        $container
+            .find('a[href*=\\#\\!]')
+            .each(function (i, a) {
+                var $a = $(a)
+                $a.attr('href', $a.attr('href').replace('#!', escapedFragmentKey))
+            });
     }
 });
 
